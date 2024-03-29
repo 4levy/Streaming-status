fd5c2b70fa645d77411e9d07034ee3a1dfa16dad
levy <100963276+4levy@users.noreply.github.com>
2024-03-30 00:42:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const server = express();
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
   const imagePath = path.join(__dirname, 'index.html');
   res.sendFile(imagePath);
 });
 
 function keepAlive() {
-  server.listen(5555, () => { console.log("Server is Ready!!" + Date.now()) });
+  server.listen(5555, () => { console.log("Started" + Date.now()) });
 }
 
 module.exports = keepAlive;
