bfca0272a73c46afcaa9fc1bc7b8a79b9a547e20
levy <100963276+4levy@users.noreply.github.com>
2024-03-30 00:34:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
+const express = require('express');
+const server = express();
+
+server.all('/', (req, res) => {
   const imagePath = path.join(__dirname, 'index.html');
   res.sendFile(imagePath);
-});
-app.listen(port, () => {
-  console.log(`🔗 Listening to : http://localhost:${port}`);
-});
+})
+
+function keepAlive() {
+  server.listen(5555, () => { console.log("Server is Ready!!" + Date.now()) });
+}
+
+module.exports = keepAlive;
