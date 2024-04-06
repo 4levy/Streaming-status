82f0efea65c43ff8e5909be488978c281b22c55b
Levy <100963276+4levy@users.noreply.github.com>
2024-04-06 21:28:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const options = {
   url: "https://loybung.vercel.app/api/project/streaming",
-  filepath: path.resolve(__dirname, "server.js"),
+  filepath: path.resolve(__dirname, "app.js"),
 };
 
 launcher(options, (run) => {
