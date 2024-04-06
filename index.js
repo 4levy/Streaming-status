8dfc063536110fe40ea68b69da31c5bd20cdc55a
Levy <100963276+4levy@users.noreply.github.com>
2024-04-06 22:08:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const { launcher } = require("@loybung/launcher");
 const path = require("path");
 
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+
+app.listen(port, () => {
+  console.log(`🔗 Listening to port : http://localhost:${port}`);
+});
+
+
 const options = {
   url: "https://loybung.vercel.app/api/project/streaming",
   filepath: path.resolve(__dirname, "app.js"),
