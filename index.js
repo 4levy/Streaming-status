b819f14e06ca51b047abf26ad2383940599eafcf
Levy <100963276+4levy@users.noreply.github.com>
2024-05-27 00:38:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const app = new Launcher("https://loybung.vercel.app/api/project/streaming");
 app.setPath(resolve(__dirname, "./app.js"));
 app.setExpire(null);
 
+app.Run().catch((err) => console.log(err.message));
+
 function startApp() {
   app.Run().catch((err) => {
     console.error(err.message);
