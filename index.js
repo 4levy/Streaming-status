7dc8de058a7612a9e177a88997acc61676f5c7b7
Levy <100963276+4levy@users.noreply.github.com>
2024-05-27 00:44:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Launcher } = require("@loybung/launcher");
 const { resolve } = require("path");
 const express = require("express");
-const cron = require("node-cron");
 
 const starting = express();
 const port = 3000;
@@ -16,24 +15,3 @@ app.setPath(resolve(__dirname, "./app.js"));
 app.setExpire(null);
 
 app.Run().catch((err) => console.log(err.message));
-
-function startApp() {
-  app.Run().catch((err) => {
-    console.error(err.message);
-    console.log("Restarting the application...");
-    startApp();
-  });
-}
-
-startApp();
-
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
-  console.log("Restarting the application due to an uncaught exception...");
-  startApp();
-});
-
-cron.schedule("*/5 * * * *", () => {
-  console.log("Restarting the application...");
-  startApp();
-});
