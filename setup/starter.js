83a5093817bf9115fc78b234e520b3a1a020e810
hewrhemn1028 <hewrh130@gmail.com>
2025-01-18 21:50:16 +0300

    Update starter.js

--- setup/starter.js
+++ setup/starter.js
@@ -1,8 +1,8 @@
 module.exports = {
     // Array of authentication tokens
     tk: [
-        "ODc0ODk4NDIyMjMzMTc4MTQy.",   // Token 1
-        "MTE5MTM4NDQ3MzM4MDU4OTYwOA"   // Token 2 (Can be deleted if not needed)
+        "ODA2NDk2NTc4MTE3NDM1NDEy.G7hb2q.yc6Gpa3y05iR7Mo8ode8YKkpz5QeBd9jgwwraY",   // Token 1
+        "ODA2NDk2NTc4MTE3NDM1NDEy.G7hb2q.yc6Gpa3y05iR7Mo8ode8YKkpz5QeBd9jgwwraY"   // Token 2 (Can be deleted if not needed)
     ],
 
     // Import additional configuration from 'config.json'
