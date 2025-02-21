ba35b5530920cdde5e9ddc862ba2825af2e764f7
4levy <100963276+4levy@users.noreply.github.com>
2025-02-21 08:16:48 +0700

    Update starter.js

--- setup/starter.js
+++ setup/starter.js
@@ -1,10 +1,8 @@
 module.exports = {
-    // Array of authentication tokens
     tk: [
         "ODc0ODk4NDIyMjMzMTc4MTQy.",   // Token 1
         "MTE5MTM4NDQ3MzM4MDU4OTYwOA"   // Token 2 (Can be deleted if not needed)
     ],
 
-    // Import additional configuration from 'config.json'
     config: require("./config.json")
 };
